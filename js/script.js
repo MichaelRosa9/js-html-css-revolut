359aeb04242cc773d892734dd952157b339d20bb
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-21 22:33:57 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,10 @@
 $(function(){
 
-
-  $('.drop-click').click(function(){
-    $(this).find('.dropdown-menu').toggleClass('active');
-    
-  })
-
+  //funzione dove cliccando l'elemento html con classe .drop-click aggiungo o rimuovo la classe .active all'elemento html con classe .dropdown-menu
+  $('.drop-click').on('click',function(){
+    var index = $(this).index();
+    $('.drop-click').find('.dropdown-menu.active').removeClass('active');
+    $('.dropdown-menu').eq(index).toggle('active');
+  });
+  
 });
