d88d1ef60f1a7d4952b667be00bd21dc301efb23
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-22 10:04:12 +0200

    correzione

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
 $(function(){
 
-  //funzione dove cliccando l'elemento html con classe .drop-click aggiungo o rimuovo la classe .active all'elemento html con classe .dropdown-menu
-  $('.drop-click').on('click',function(){
-    var index = $(this).index();
-    $('.drop-click').find('.dropdown-menu.active').removeClass('active');
-    $('.dropdown-menu').eq(index).toggle('active');
-  });
+  //funzione hover di jquery ull'elemento .drop-click
+  $('.drop-click').mouseenter(function(){
+    //tolgo la classe .active a tutti gli elementi con classe .dropdown-menu.active
+    $('.dropdown-menu.active').removeClass('active');
+    //aggiungo la classe .active all'elemento puntato dal mouse
+    $(this).children('.dropdown-menu').addClass('active');
+  })
   
+  //funzione quando clicco l'elemento .drop-click
+  $('.drop-click').click(function(){
+    //l'elemento cliccato aggiunge o rimiove la classe .active
+    $(this).children('.dropdown-menu').toggleClass('active');
+  })
 });
